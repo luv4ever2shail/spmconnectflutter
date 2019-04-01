907498d0a812b0a08f7e0b5409e3c2120e6487d0
Shail Patel <shail@spm-automation.com>
2019-04-01 15:58:12 -0400

    first commit to dev

---
+++ lib/screens/report_list.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:spmconnectapp/screens/report_detail.dart';
+
+class ReportList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _ReportList();
+  }
+}
+
+class _ReportList extends State<ReportList> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SPM Connect Service Reports'),
+      ),
+      body: getReportListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint('FAB clicked');
+          navigateToDetail('Add New Report');
+        },
+        tooltip: 'Create New Report',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  ListView getReportListView() {
+    TextStyle titleStyle = Theme.of(context).textTheme.subhead;
+
+    return ListView.builder(
+      itemCount: count,
+      itemBuilder: (BuildContext context, int position) {
+        return Card(
+          color: Colors.white,
+          elevation: 2.0,
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.yellow,
+              child: Icon(Icons.keyboard_arrow_right),
+            ),
+            title: Text(
+              'Dummy Title',
+              style: titleStyle,
+            ),
+            subtitle: Text('Dummy Date'),
+            trailing: Icon(
+              Icons.delete,
+              color: Colors.grey,
+            ),
+            onTap: () {
+              debugPrint("ListTile Tapped");
+              navigateToDetail('Edit Report');
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  void navigateToDetail(String title) {
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return ReportDetail(title);
+    }));
+  }
+}
