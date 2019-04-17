2748cc0ca447a63af72b133d608a281019e3c0f1
Shail Patel <shailpatel1996@gmail.com>
2019-04-17 18:42:52 -0400

    moved the location of rest api

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -3,7 +3,7 @@ import 'package:aad_oauth/model/config.dart';
 import 'package:flutter/material.dart';
 import 'package:spmconnectapp/API_Keys/api.dart';
 import 'package:spmconnectapp/screens/home.dart';
-import 'package:spmconnectapp/screens/restapi.dart';
+import 'package:spmconnectapp/models/restapi.dart';
 import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'dart:async';
 
